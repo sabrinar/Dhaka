e117bfeb51ec9d93575eedb9121c021a9b77eaab
sabrinar <sabrinar@users.noreply.github.com>
2017-08-03 16:53:01 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 10:46:10 2017
+
+@author: t-sarash
+"""
+
+from setuptools import setup, find_packages
+
+
+
+
+setup(name='autoencoder',
+      version='0.1',
+      description='The funniest joke in the world',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
